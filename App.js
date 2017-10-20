d2345a80dff4476b611ff8219fc6428a0257970c
theoJA <theophilus.ja@gmail.com>
2017-10-19 20:32:36 -0400

    Added RootNav

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AuthScreen from './src/components/auth/AuthScreen'
+import { RootNav } from './src/components/navigation/RootNav';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <AuthScreen />
-      </View>
+      <RootNav />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
