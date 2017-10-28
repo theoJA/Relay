ab951d8118aa67fad1f071137765cda06e50be58
theoJA <theophilus.ja@gmail.com>
2017-10-28 15:34:09 -0400

    First commit. For rendering a flat list of subjects/fields

---
+++ src/components/common/RenderFlatList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, FlatList } from "react-native";
+import { CardSection } from '../common';
+import { AddRemoveButton } from '../common/AddRemoveButton'
+
+export class RenderFlatList extends React.Component {
+
+  _keyExtractor = (item, subject) => item.subject; 
+
+  render() {
+    return (
+      <FlatList 
+        data={this.props.content}
+        keyExtractor={this._keyExtractor}
+        renderItem={
+          ({item}) => 
+            <CardSection style={Styles.container}>
+              <Text style={Styles.descStyle}>{item.subject}</Text>
+              <AddRemoveButton 
+                id={item.subject}
+              />
+            </CardSection>
+        }
+      />
+    )
+  }
+}
+
+const Styles = {
+  container: {
+    flex: 1
+  },
+  descStyle: {
+    fontSize: 15,
+    paddingLeft: 18,
+    flex: 5
+  }
+}
