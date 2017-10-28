bc4136952b76b5474d805a62fc2130bece73fc92
theoJA <theophilus.ja@gmail.com>
2017-10-28 15:29:46 -0400

    Added arrays containing program and subjects

---
+++ src/Programs_Subjects.js
@@ -0,0 +1,33 @@
+export const programs = [
+  { name: 'COMPUTER SCIENCE' },
+  { name: 'ENGINEERING'}
+]
+
+export const computerScience = [
+  { key: '1' , subject: 'General Computer Science' },
+  { key: '2' , subject: 'Discrete Structures' },
+  { key: '3' , subject: 'Systems Programming' },
+  { key: '4' , subject: 'Data Structures and Algorithms' },
+  { key: '5' , subject: 'Software Engineering' },
+  { key: '6' , subject: 'Web Applications' },
+  { key: '7' , subject: 'Mobile Applications' },
+  { key: '8' , subject: 'Operating Systems' },
+  { key: '9' , subject: 'Object-Oriented Programming' },
+  { key: '10' ,subject: 'Databases' },
+  { key: '11' ,subject: 'Human-Computer Interaction' },
+  { key: '12' , subject: 'Programming Paradigms' },
+  { key: '13' , subject: 'Computer Networks' },
+  { key: '14' , subject: 'Advanced Algorithms' },
+  { key: '15' , subject: 'Artificial Intelligence' },
+  { key: '16' , subject: 'Cryptography' },
+  { key: '17' , subject: 'Security' }
+];
+
+export const engineering = [
+  { key: '18' , subject: 'Mechanical' },
+  { key: '19' , subject: 'Electrical' },
+  { key: '20' , subject: 'Environmental' },
+  { key: '19' , subject: 'Aerospace' },
+  { key: '18' , subject: 'Biomedical and Mechanical' },
+  { key: '18' , subject: 'Biomedical and Electrical' }
+];
