91aea8b5fde645d51e42ce0c8271b6215e5a0fb3
theoJA <theophilus.ja@gmail.com>
2017-10-30 22:53:51 -0400

    Gave a lot of styling to the button

--- src/components/common/OauthButton.js
+++ src/components/common/OauthButton.js
@@ -3,11 +3,40 @@ import { View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const OauthButton = props => {
-  const { name, size, color } = props
+  const { name, size, color, buttonColor, buttonTitle } = props
 
   return (
-    <Ionicons name={name} size={size} color={color} />
+    <View style={[Styles.buttonStyle, {backgroundColor: buttonColor}]}>
+      <Ionicons style={Styles.logoStyle} name={name} size={size} color={color} />
+      <Text style={Styles.buttonTitleStyle}>{buttonTitle}</Text>
+    </View>
   )
 }
 
 export {OauthButton}
+
+const Styles = {
+  buttonStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginBottom: 15,
+    marginRight: 40,
+    marginLeft: 40,
+    borderRadius: 5
+  },
+  logoStyle: {
+    flex: 1,
+    borderRightWidth: 1,
+    marginLeft: 10
+  },
+  buttonTitleStyle: {
+    flex: 3,
+    fontWeight: 'bold',
+    paddingLeft: 20,
+    color: '#fff'
+  }
+}
