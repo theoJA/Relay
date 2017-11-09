47ff0a3e36cf6888365c8c78aba142ac605a4624
theoJA <theophilus.ja@gmail.com>
2017-11-08 23:47:42 -0500

    Navbar. Might not be useful

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 
 export default class NavBar extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Welcome to the home screen</Text>
+      <View style={Styles.container}>
+      
+        <View style={Styles.menuStyle}>
+          <TouchableOpacity>
+            <Ionicons style={Styles.iconStyle} name="md-menu" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+          
+        <View style={Styles.navbarItemStyle}>
+          <TouchableOpacity>
+            <Ionicons style={Styles.iconStyle} name="ios-add-circle" size={32} color="white" />
+          </TouchableOpacity>
+            
+          <TouchableOpacity>
+            <Ionicons style={Styles.iconStyle} name="ios-funnel" size={32} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Ionicons style={Styles.iconStyle} name="md-search" size={32} color="white" />
+          </TouchableOpacity>
+        </View> 
       </View>
     )
   }
@@ -15,8 +34,21 @@ export default class NavBar extends Component {
 
 const Styles = {
   container: {
-    flex: 1,
     flexDirection: 'row',
-    backgroundColor: '#ECEFF1',
+    alignItems: 'center',
+    backgroundColor: '#273746',
+    marginTop: Expo.Constants.statusBarHeight,
+    height: 56,
+  },
+  menuStyle: {
+    flex: 3,
+  },
+  navbarItemStyle: {
+    flexDirection: 'row',
+    flex: 2,
+  },
+  iconStyle: {
+    padding: 10
   }
+
 }
