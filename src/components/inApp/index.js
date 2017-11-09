9e716731becdfb24c67786a1c37f336d3eafc8cb
theoJA <theophilus.ja@gmail.com>
2017-11-08 23:48:49 -0500

    Added new components

--- src/components/inApp/index.js
+++ src/components/inApp/index.js
@@ -1,5 +1,11 @@
 import Home from './Home';
+import MyNotes from './MyNotes';
+import Bookmarked from './Bookmarked';
+import AppSettings from './AppSettings';
 
 export {
-  Home
+  Home,
+  MyNotes,
+  Bookmarked,
+  AppSettings
 }
