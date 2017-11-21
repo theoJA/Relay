a5aaa9de62be9a1a85ecee1c94d55f51ba0551b7
theoJA <theophilus.ja@gmail.com>
2017-11-21 13:02:52 -0500

    Added modal for inserting link

--- src/components/common/NoteEditor.js
+++ src/components/common/NoteEditor.js
@@ -1,34 +1,59 @@
 import React, { Component } from "react";
 import { 
   StyleSheet, Image, View, Button, Text, TouchableOpacity, 
-  TextInput, ScrollView, KeyboardAvoidingView, WebView } from "react-native";
+  TextInput, ScrollView } from "react-native";
+import Modal from 'react-native-modal';
 import { Ionicons, Entypo, MaterialCommunityIcons, EvilIcons } from '@expo/vector-icons';
 import { ImagePicker } from 'expo';
 
+
+const colors = {
+  inactive: '#ECEFF1',
+  active: '#AEB6BF'
+};
+
+const textStyles = {
+  normal: { fontSize: 15, fontWeight: 'normal', fontStyle: 'normal'},
+  H1: { fontSize: 25, fontWeight: 'bold', fontStyle: 'normal'},
+  H2: { fontSize: 20, fontWeight: 'bold', fontStyle: 'normal'},
+  script: { fontSize: 15, backgroundColor: '#AED6F1', fontWeight: 'normal', fontStyle: 'normal' },
+  quote: { fontSize: 17, fontStyle: 'italic', fontWeight: 'normal'},
+};
+
+const initialButtonStates = {
+  H1: {active: false, backgroundColor: colors.inactive},
+  H2: {active: false, backgroundColor: colors.inactive},
+  script: {active: false, backgroundColor: colors.inactive},
+  quote: {active: false, backgroundColor: colors.inactive},
+};
+
 export default class NoteEditor extends Component {
   
+
   constructor(props) {
     super(props);
     this.state = {
-      // text: '',
-      // textInputValue: '',
-      // image: null,
-      // typeArr: [],
-      // dataArr: [],
-      // editorStates: [],
-      // undoRedo: 1,
-      // currId: 0,
-      // renderedStuff: [],
+      isModalVisible: false,
+      buttonStates: initialButtonStates,
+      link: null,
       data: null,
       textInputValue: null,
       editorState: {
         typeArr: [],
         dataArr: [],
+        styleArr: [],
       },
       tagsArr: []
     }
   }
 
+  _showModal() {
+    this.setState({isModalVisible: true});
+  }
+
+  _hideModal() {
+    this.setState({isModalVisible: false});
+  }
 
   _insertImage = async () => {
     let { data, editorState } = this.state; 
@@ -36,7 +61,6 @@ export default class NoteEditor extends Component {
       allowsEditing: true,
       aspect: [16, 9],
     });
-    //console.log(result);
     if (!result.cancelled) {
 
       editorState.typeArr.push('image');
@@ -51,15 +75,12 @@ export default class NoteEditor extends Component {
         },
         data: null,
       });
-      
     }
-    
     this._dataRenderer(editorState);
   };
 
   _insertText() {
     let { data, editorState, textInputValue } = this.state; 
-    //let { text, image, typeArr, dataArr, editorStates, undoRedo, currId } = this.state;
     
     if (!data) {
       return;
@@ -86,87 +107,20 @@ export default class NoteEditor extends Component {
       this._dataRenderer(editorState);
     }
 
-    // IF undoRedo is 1, we add a new data object to the array. We use the same case when editing a paragraph or image
-    // if (undoRedo === 1) {
-      
-    //   // Push 'text' into the typeArr
-    //   typeArr.push('text');
-    //   let tempTypeArr = typeArr;
-      
-    //   // Push the text into dataArr
-    //   dataArr.push(text);
-    //   let tempDataArr = dataArr;
+  }
 
-    //   editorStates.push({
-    //     editorTypeArr: [],
-    //     editorDataArr: []
-    //   });
-
-    //   editorStates[editorStates.length - undoRedo].editorTypeArr.push(...typeArr);
-    //   editorStates[editorStates.length - undoRedo].editorDataArr.push(...dataArr);
-
-    //   let tempEditorStates = editorStates;
-
-    //   this.setState({
-    //     typeArr: tempTypeArr,
-    //     dataArr: tempDataArr,
-    //     editorStates: tempEditorStates
-    //   })
-    // }
-    
-    // ELSE if undoRedo is more than 1, we just modify the next object to the right in the array
-    // else if (undoRedo > 1) {
-      
-    //   // insert 'text' and its data into typeArr and dataArr respectively
-    //   typeArr[editorStates.length - undoRedo + 1] = 'text';
-    //   dataArr[editorStates.length - undoRedo + 1] = text;
-      
-    //   // slice the editorStates to remove everything after the current state
-    //   let tempEditorStates = editorStates.slice(0, editorStates.length - undoRedo + 1);
-    //   this.setState({ editorStates: tempEditorStates });
-
-    //   // set undoRedo to 1 since we cannot redo once we add a new state
-    //   this.setState({ undoRedo: 1 });
-
-    //   // push an empty object into editorStates
-    //   editorStates.push({
-    //     editorTypeArr: [],
-    //     editorDataArr: []
-    //   });
-
-    //   // slice the typeArr and dataArr
-    //   let tempTypeArr = typeArr.slice(0,editorStates.length - undoRedo + 1);
-    //   let tempDataArr = dataArr.slice(0,editorStates.length - undoRedo + 1);
-
-    //   this.setState({ typeArr : tempTypeArr });
-    //   this.setState({ dataArr : tempDataArr });
-
-    //   // push newly sliced typeArr and dataArr into the new object's array
-    //   editorStates[editorStates.length - 1].editorTypeArr.push(typeArr);
-    //   editorStates[editorStates.length - 1].editorDataArr.push(dataArr);
-
-    //}
-
-    // after processing the new data and types, we call the renderer with the newly added state
-    //this._dataRenderer(editorStates[editorStates.length - 1]);
+  _insertLink() {
+    alert('Insert a link!');
   }
 
   _displayKey() {
-    
     alert(this.id.split(',')[0]);
   }
 
   _dataRenderer(editorStateObj) {
-    
     let { data, editorStates, tagsArr } = this.state;
 
-    // let tempTagsArr = tagsArr;
-    // tempTagsArr.length = 0;
-    // this.setState({tagsArr: tempTagsArr});
-    
-      
     tagsArr.length = 0;
-    
     editorStateObj.typeArr.map((type,index) => {
       
       switch (type) {
@@ -211,13 +165,43 @@ export default class NoteEditor extends Component {
     this.setState({ tagsArr: tempTagsArr });
   }
 
+  _styleOutput() {
+    let { H1, H2, script, quote, link } = this.state.buttonStates;
+    if (H1.active) {return textStyles.H1;}
+    if (H2.active) {return textStyles.H2;}
+    if (script.active) {return textStyles.script;}
+    if (quote.active) {return textStyles.quote;}
+    else return textStyles.normal;
+  }
+
+  _changeButtonColor(id) {
+    let { buttonStates } = this.state;
+    if(buttonStates[id].active) {
+      buttonStates[id].active = false;
+      buttonStates[id].backgroundColor = colors.inactive;
+    } else {
+      buttonStates.H1.active = false;
+      buttonStates.H2.active = false;
+      buttonStates.script.active = false;
+      buttonStates.quote.active = false;
+      buttonStates.H1.backgroundColor = colors.inactive;
+      buttonStates.H2.backgroundColor = colors.inactive;
+      buttonStates.script.backgroundColor = colors.inactive;
+      buttonStates.quote.backgroundColor = colors.inactive;
+
+      buttonStates[id].active = true;
+      buttonStates[id].backgroundColor = colors.active;
+    }
+    this.forceUpdate();
+  }
+
   render() {
-    let { editorState, data, textInputValue, tagsArr } = this.state;
+    let { editorState, data, textInputValue, tagsArr, buttonStates } = this.state;
     return (
     
       <View style={Styles.container}>
         <TextInput 
-        style={Styles.textInput} 
+        style={[Styles.textInput, this._styleOutput()]} 
         autoCapitalize={'sentences'}
         multiline={true} 
         numberOfLines={5} 
@@ -229,33 +213,40 @@ export default class NoteEditor extends Component {
         value={textInputValue} />
 
         <View style={Styles.editorButtonsContainer}>
-          <TouchableOpacity>
-            <MaterialCommunityIcons style={Styles.editorButtons} name="format-header-1" size={20} color="black" />
+          <TouchableOpacity onPress={() => {
+            this._changeButtonColor('H1');
+          }}>
+            <MaterialCommunityIcons style={[Styles.editorButtons, {backgroundColor: `${buttonStates.H1.backgroundColor}`}]} name="format-header-1" size={20} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <MaterialCommunityIcons style={Styles.editorButtons} name="format-header-2" size={20} color="black" />
+
+          <TouchableOpacity onPress={() => {
+            this._changeButtonColor('H2');
+          }}>
+            <MaterialCommunityIcons style={[Styles.editorButtons, {backgroundColor: `${buttonStates.H2.backgroundColor}`}]} name="format-header-2" size={20} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Entypo style={Styles.editorButtons} name="code" size={20} color="black" />
+
+          <TouchableOpacity onPress={() => {
+            this._changeButtonColor('script');
+          }}>
+            <Entypo style={[Styles.editorButtons, {backgroundColor: `${buttonStates.script.backgroundColor}`}]} name="code" size={20} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Entypo style={Styles.editorButtons} name="quote" size={20} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Entypo style={Styles.editorButtons} name="link" size={20} color="black" />
+
+          <TouchableOpacity onPress={() => {
+            this._changeButtonColor('quote');
+          }}>
+            <Entypo style={[Styles.editorButtons, {backgroundColor: `${buttonStates.quote.backgroundColor}`}]} name="quote" size={20} color="black" />
           </TouchableOpacity>
         </View>
 
         <View style={Styles.editorButtonsContainer}>
-          <TouchableOpacity
-          onPress={
-              this._insertText.bind(this)
-          }
-          >
-            <Text style={Styles.editorButtons}>Insert Text</Text>
+          <TouchableOpacity onPress={this._insertText.bind(this)}>
+            <Text style={Styles.editorButtons}>+ Text</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={this._insertImage}>
-            <Text style={Styles.editorButtons}>Insert Image</Text>
+            <Text style={Styles.editorButtons}>+ Image</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._showModal.bind(this)}>
+            <Text style={Styles.editorButtons}>+ Link</Text>
           </TouchableOpacity>
         </View>
 
@@ -273,6 +264,32 @@ export default class NoteEditor extends Component {
           </View>
           )}
         </ScrollView>
+
+        <Modal isVisible={this.state.isModalVisible}>
+          <View style={Styles.modalContainer}>
+            
+            <Text style={Styles.textInputTitleStyle}>
+              Add a link
+            </Text>
+            <TextInput 
+            style={Styles.textInputStyle}
+            autoCapitalize={'sentences'}
+            multiline={true} 
+            numberOfLines={2} 
+            onChangeText={(text) => this.setState({ link: text })}
+            value={this.state.link}
+            />
+
+            <View style={{flexDirection: 'row', marginTop: 20}}>
+              <TouchableOpacity onPress={this._hideModal.bind(this)}>
+                <Text style={[Styles.modalButton, { backgroundColor: "#EF9A9A" }]}>Back</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this._insertLink}>
+                <Text style={[Styles.modalButton, { backgroundColor: "#C5E1A5" }]}>Add</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   };
@@ -302,7 +319,31 @@ const Styles = StyleSheet.create({
     borderRadius: 5, 
     borderWidth: 1, 
     padding: 8,
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    margin: 30,
+    borderRadius: 5,
+    justifyContent: 'flex-start',
+  },
+  modalButton: {
+    borderRadius: 5,
+    padding: 5,
+    textAlign: 'center',
+    margin: 10
+  },
+  textInputTitleStyle: {
+    color: "#566573",
+    fontSize: 16,
+    marginTop: 20,
     fontWeight: 'bold'
+  },
+  textInputStyle: {
+    paddingTop: 5,
+    paddingBottom: 10,
+    padding: 5,
   },
 
 });
