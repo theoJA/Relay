870111ff8e58d5f7adc2291ad51b1f25ac2f1475
theoJA <theophilus.ja@gmail.com>
2017-11-22 18:00:18 -0500

    Styling for text now appears in created note

--- src/components/common/NoteEditor.js
+++ src/components/common/NoteEditor.js
@@ -17,7 +17,8 @@ const textStyles = {
   H1: { fontSize: 25, fontWeight: 'bold', fontStyle: 'normal'},
   H2: { fontSize: 20, fontWeight: 'bold', fontStyle: 'normal'},
   script: { fontSize: 15, backgroundColor: '#AED6F1', fontWeight: 'normal', fontStyle: 'normal' },
-  quote: { fontSize: 17, fontStyle: 'italic', fontWeight: 'normal'},
+  quote: { fontSize: 17, fontStyle: 'italic', fontWeight: 'normal', paddingLeft: 20, paddingRight: 20, paddingTop: 5, paddingBottom: 5 },
+  link: { fontSize: 17, textDecorationLine: 'underline' },
 };
 
 const initialButtonStates = {
@@ -35,8 +36,9 @@ export default class NoteEditor extends Component {
     this.state = {
       isModalVisible: false,
       buttonStates: initialButtonStates,
-      link: null,
+      activeTextStyle: 'normal',
       data: null,
+      linkInputValue: null,
       textInputValue: null,
       editorState: {
         typeArr: [],
@@ -59,7 +61,7 @@ export default class NoteEditor extends Component {
     let { data, editorState } = this.state; 
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      aspect: [16, 9],
+      aspect: [4, 3],
     });
     if (!result.cancelled) {
 
@@ -68,10 +70,15 @@ export default class NoteEditor extends Component {
       
       editorState.dataArr.push(result.uri);
       let tempDataArr = editorState.dataArr;
+
+      editorState.styleArr.push({});
+      let tempStyleArr = editorState.styleArr;
+
       this.setState({
         editorState: {
           typeArr: tempTypeArr,
           dataArr: tempDataArr,
+          styleArr: tempStyleArr,
         },
         data: null,
       });
@@ -80,7 +87,7 @@ export default class NoteEditor extends Component {
   };
 
   _insertText() {
-    let { data, editorState, textInputValue } = this.state; 
+    let { data, editorState, textInputValue, buttonStates, activeTextStyle } = this.state; 
     
     if (!data) {
       return;
@@ -93,12 +100,14 @@ export default class NoteEditor extends Component {
       editorState.dataArr.push(data);
       let tempDataArr = editorState.dataArr;
   
-      // Save the style as well!
+      editorState.styleArr.push(textStyles[activeTextStyle]);
+      let tempStyleArr = editorState.styleArr;
   
       this.setState({
         editorState: {
           typeArr: tempTypeArr,
           dataArr: tempDataArr,
+          styleArr: tempStyleArr,
         },
         textInputValue: null,
         data: null,
@@ -110,15 +119,42 @@ export default class NoteEditor extends Component {
   }
 
   _insertLink() {
-    alert('Insert a link!');
+    let { linkInputValue, editorState, textInputValue } = this.state; 
+    
+    if (!linkInputValue) {
+      return;
+    }
+
+    else {
+      editorState.typeArr.push('link');
+      let tempTypeArr = editorState.typeArr;
+      
+      editorState.dataArr.push(linkInputValue);
+      let tempDataArr = editorState.dataArr;
+
+      editorState.styleArr.push(textStyles.link);
+      let tempStyleArr = editorState.styleArr;
+  
+      this.setState({
+        editorState: {
+          typeArr: tempTypeArr,
+          dataArr: tempDataArr,
+          styleArr: tempStyleArr,
+        },
+        linkInputValue: null
+      })
+  
+      this._dataRenderer(editorState);
+    }
   }
 
   _displayKey() {
-    alert(this.id.split(',')[0]);
+    //alert(this.id.split(',')[0]);
+    alert(this.id);
   }
 
   _dataRenderer(editorStateObj) {
-    let { data, editorStates, tagsArr } = this.state;
+    let { tagsArr } = this.state;
 
     tagsArr.length = 0;
     editorStateObj.typeArr.map((type,index) => {
@@ -131,7 +167,7 @@ export default class NoteEditor extends Component {
               id={`${type},${editorStateObj.dataArr[index]},${index}`}
               onPress={this._displayKey}
             >
-              <Text>
+              <Text style={editorStateObj.styleArr[index]}>
                 {`${editorStateObj.dataArr[index]}\n\n`}
               </Text>
             </TouchableOpacity>
@@ -148,7 +184,7 @@ export default class NoteEditor extends Component {
             >
               <Image
                 source={{ uri: editorStateObj.dataArr[index] }}
-                style={{ width: 300, height: 250, margin: 10 }}
+                style={[{ width: 300, height: 250, margin: 10 }, editorStateObj.styleArr[index]]}
               />
             </TouchableOpacity>
             );
@@ -156,6 +192,18 @@ export default class NoteEditor extends Component {
           }
           
         case "link": {
+          tagsArr.push(
+            <TouchableOpacity 
+              key={`${type},${editorStateObj.dataArr[index]},${index}`}
+              id={`${type},${editorStateObj.dataArr[index]},${index}`}
+              onPress={this._displayKey}
+            >
+              <Text style={editorStateObj.styleArr[index]}>
+                {`${editorStateObj.dataArr[index]}\n\n`}
+              </Text>
+            </TouchableOpacity>
+          );
+          break;
         }
       }
     });  
@@ -175,10 +223,11 @@ export default class NoteEditor extends Component {
   }
 
   _changeButtonColor(id) {
-    let { buttonStates } = this.state;
+    let { buttonStates, activeTextStyle } = this.state;
     if(buttonStates[id].active) {
       buttonStates[id].active = false;
       buttonStates[id].backgroundColor = colors.inactive;
+      this.setState({activeTextStyle: 'normal'})
     } else {
       buttonStates.H1.active = false;
       buttonStates.H2.active = false;
@@ -191,6 +240,7 @@ export default class NoteEditor extends Component {
 
       buttonStates[id].active = true;
       buttonStates[id].backgroundColor = colors.active;
+      this.setState({activeTextStyle: id})
     }
     this.forceUpdate();
   }
@@ -276,15 +326,15 @@ export default class NoteEditor extends Component {
             autoCapitalize={'sentences'}
             multiline={true} 
             numberOfLines={2} 
-            onChangeText={(text) => this.setState({ link: text })}
-            value={this.state.link}
+            onChangeText={(text) => this.setState({ linkInputValue: text })}
+            value={this.state.linkInputValue}
             />
 
             <View style={{flexDirection: 'row', marginTop: 20}}>
               <TouchableOpacity onPress={this._hideModal.bind(this)}>
                 <Text style={[Styles.modalButton, { backgroundColor: "#EF9A9A" }]}>Back</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={this._insertLink}>
+              <TouchableOpacity onPress={this._insertLink.bind(this)}>
                 <Text style={[Styles.modalButton, { backgroundColor: "#C5E1A5" }]}>Add</Text>
               </TouchableOpacity>
             </View>
@@ -330,14 +380,17 @@ const Styles = StyleSheet.create({
   },
   modalButton: {
     borderRadius: 5,
-    padding: 5,
+    borderWidth: 1, 
+    padding: 8,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
+    fontWeight: 'bold'
   },
   textInputTitleStyle: {
     color: "#566573",
     fontSize: 16,
     marginTop: 20,
+    marginLeft: 5,
     fontWeight: 'bold'
   },
   textInputStyle: {
