331f1b8d8d2d232e4bb43fc389c54402483a277c
theoJA <theophilus.ja@gmail.com>
2017-12-10 22:34:23 -0500

    state reducers

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,43 @@
+import {
+  EMAIL_CHANGED, 
+  PASSWORD_CHANGED,
+  SIGN_IN_EMAIL,
+  SIGN_IN_EMAIL_SUCCESS,
+  SIGN_IN_EMAIL_FAIL
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: '',
+  loading: false
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload}
+
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload}
+
+    case SIGN_IN_EMAIL:
+      return { ...state, loading: true, error: '' }
+
+    case SIGN_IN_EMAIL_SUCCESS: 
+      return { ...state, ...INITIAL_STATE, user: action.payload }
+
+    case SIGN_IN_EMAIL_FAIL:
+      return {
+        ...state,
+        error: 'Authentication Failed',
+        password: '',
+        loading: false
+      }
+
+    default:
+      return state;
+  }
+}
