167badf21256e6438a21711513917785c1d72476
theoJA <theophilus.ja@gmail.com>
2017-12-10 22:33:51 -0500

    all dispatch types in here

---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+// auth purposes
+export const EMAIL_CHANGED = 'email_changed'
+export const PASSWORD_CHANGED = 'password_changed'
+export const SIGN_IN_EMAIL = 'sign_in_email'
+export const SIGN_IN_EMAIL_SUCCESS = 'sign_in_email_success'
+export const SIGN_IN_EMAIL_FAIL = 'sign_in_email_fail'
+
+export const USERNAME_CHANGED = 'username_changed'
