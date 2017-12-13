34c97c23727b138155c9d37d266012b40e61fa94
theoJA <theophilus.ja@gmail.com>
2017-12-12 21:40:51 -0500

    Deleted

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import AuthReducer from "./AuthReducer";
-import InAppReducer from "./InAppReducer";
-
-export default combineReducers({
-  auth: AuthReducer,
-  inApp: InAppReducer
-});
