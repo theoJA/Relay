1f7373667cf7fa29ad1371675f6fbe92a63bd6bb
theoJA <theophilus.ja@gmail.com>
2017-12-13 22:37:36 -0500

    adding tags func added :tada: :squirrel: :tada:

--- src/components/inApp/CreateNote.js
+++ src/components/inApp/CreateNote.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, Button } from "react-native";
+import firebase from "firebase";
+import Autocomplete from "react-native-autocomplete-input";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, Button, ScrollView } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import Modal from 'react-native-modal';
 import NoteEditor from '../richTextEditor/NoteEditor'
+import { programs, computerScience, engineering } from "../../Programs_Subjects";
+
+
+const SECTIONS = [
+  {
+    title: programs[0].name,
+    content: computerScience,
+  },
+  {
+    title: programs[1].name,
+    content: engineering,
+  }
+];
 
 export default class CreateNote extends Component {
 
@@ -11,7 +26,12 @@ export default class CreateNote extends Component {
     this.state = {
       isModalVisible: false,
       title: null,
-      tags: []
+      tag: '',
+      query: '',
+      tagsArr: [],
+      userInterests: [],
+      addedInterests: [],
+      profileUid: ''
     }
   }
 
@@ -23,8 +43,20 @@ export default class CreateNote extends Component {
     this.setState({isModalVisible: false});
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    let { currentUser } = firebase.auth();
+    let interestArr = SECTIONS[0].content.concat(SECTIONS[1].content);
     this.props.navigation.setParams({ showModal: this._showModal.bind(this) })
+
+
+    firebase.database().ref(`/users/${currentUser.uid}/profile`)
+    .on('value', snapshot => {
+      let profileId = Object.keys(snapshot.val())[0];
+      this.setState({
+        profileUid: profileId,
+        userInterests: interestArr
+      });
+    });
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -34,8 +66,8 @@ export default class CreateNote extends Component {
       <TouchableOpacity
       onPress={params.showModal}
       >
-        <View style={{marginRight: 10, borderColor: '#fff', borderRadius: 5, borderWidth: 1, padding: 5}}>
-          <Text style={{fontSize: 16, color: '#fff'}}>
+        <View style={{marginRight: 15 }}>
+          <Text style={{fontSize: 16, backgroundColor: '#fff', borderColor: '#fff', borderRadius: 2, borderWidth: 1, padding: 5}}>
             Next
           </Text>
         </View>
@@ -43,6 +75,7 @@ export default class CreateNote extends Component {
     )
 
     return {
+      title: 'Create article',
       headerTintColor: 'white',
       headerStyle: { backgroundColor: '#273746', marginTop: Expo.Constants.statusBarHeight },
       headerTitleStyle: { color: 'white'},
@@ -50,8 +83,38 @@ export default class CreateNote extends Component {
     };
   };
 
+  _findTag(query) {
+    if (query === '') {
+      return [];
+    }
+    const { userInterests } = this.state;
+    const regex = new RegExp(`${query}`);
+    return userInterests.filter(interest => interest.subject.search(regex) >= 0);
+  }
+
+  _addTags() {
+    let { addedInterests, query } = this.state;
+    addedInterests.push(query);
+    this.setState({
+      addedInterests,
+      query: ''
+    });
+  }
+
+  _removeTags() {
+    let { addedInterests } = this.that.state;
+    let removeIndex = addedInterests.indexOf(this.id);
+    addedInterests.splice(removeIndex, 1);
+    this.that.setState({
+      addedInterests
+    });
+  }
 
   render() {
+    const { query, addedInterests } = this.state;
+    const interests = this._findTag(query);
+    const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();    
+
     return (
       <View style={Styles.container}>
 
@@ -59,11 +122,8 @@ export default class CreateNote extends Component {
 
         <Modal isVisible={this.state.isModalVisible}>
           <View style={Styles.modalContainer}>
-            <View style={{ borderBottomWidth: 1, borderColor: '#ddd', }}>
-              <Text style={[Styles.textInputTitleStyle, {marginTop: 5, marginBottom: 10, fontSize: 18 }]}>Title and Tags</Text>
-            </View>
 
-            <Text style={Styles.textInputTitleStyle}>
+            <Text style={[Styles.textInputTitleStyle, { marginTop: 10 }]}>
               TITLE
             </Text>
             <TextInput 
@@ -78,26 +138,56 @@ export default class CreateNote extends Component {
             <Text style={Styles.textInputTitleStyle}>
               TAGS
             </Text>
-            <View style={{flexDirection: 'row'}}>
-              <TextInput 
-              style={[Styles.textInputStyle,{ flex: 7, fontSize: 13 }]}
-              autoCapitalize={'sentences'}
-              multiline={true} 
-              numberOfLines={1} 
-              onChangeText={(text) => this.setState({ title: text })}
-              value={this.state.title}
+            <View style={{ flexDirection: 'row' }}>
+              <Autocomplete 
+                containerStyle={[Styles.textInputStyle,{ flex: 7 }]}
+                autoCapitalize="words"
+                autoCorrect={false}
+                data={interests.length === 1 && comp(query, interests[0].subject) ? [] : interests}
+                defaultValue={query}
+                onChangeText={text => this.setState({ query: text })}
+                placeholder="Enter tag"
+                renderItem={({ subject }) => (
+                  <TouchableOpacity onPress={() => this.setState({ query: subject })}>
+                    <Text style={{ margin: 5 }}>
+                      {subject}
+                    </Text>
+                  </TouchableOpacity>
+                )}
               />
-              <TouchableOpacity onPress={() => {}} style={{ flex: 5 }}>
-                <Text style={[Styles.modalButton, { borderColor: '#000', backgroundColor: '#ddd', borderRadius: 5, borderWidth: 1, }]}>Add Tag</Text>
+              <TouchableOpacity onPress={() => {}} style={{ flex: 3 }}
+                onPress={this._addTags.bind(this)}
+              >
+                <Text style={[Styles.modalButton, { backgroundColor: '#000', color: '#fff', borderWidth: 1, paddingTop: -2, paddingBottom: -2, paddingLeft: -2, paddingRight: -2, fontSize: 20 }]}>+</Text>
               </TouchableOpacity>
             </View>
 
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap'}}>   
+              {(
+                addedInterests.map(interest => {
+                  return (
+                    <TouchableOpacity
+                      style={{}}
+                      key={interest}
+                      id={interest}
+                      that={this}
+                      onPress={this._removeTags}
+                    >
+                      <Text style={{ padding: 2, margin: 1, fontSize: 11, borderWidth: 1, borderRadius: 2 }}>{interest + ' -'}</Text>
+                    </TouchableOpacity>
+                  )
+                })
+              )
+              }
+            </View>
+            
+
             <View style={{flexDirection: 'row', marginTop: 20}}>
               <TouchableOpacity onPress={this._hideModal.bind(this)}>
-                <Text style={[Styles.modalButton, { backgroundColor: '#AED6F1' }]}>Back</Text>
+                <Text style={[Styles.modalButton, { backgroundColor: '#000', color: '#fff' }]}>Back</Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={() => {this.props.navigation.goBack()}}>
-                <Text style={[Styles.modalButton, { backgroundColor: "#C5E1A5" }]}>Publish</Text>
+                <Text style={[Styles.modalButton, { backgroundColor: "#9CCC65", borderColor: "#9CCC65", color: '#fff' }]}>Publish</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -108,6 +198,27 @@ export default class CreateNote extends Component {
     )
   }
 }
+
+/*
+<Autocomplete 
+              style={[Styles.textInputStyle,{ flex: 7, fontSize: 13 }]}
+              autoCapitalize="none"
+              autoCorrect={false}
+              multiline={true} 
+              numberOfLines={1} 
+              data={interests.length === 1 && comp(query, interests[0].subject) ? [] : interests}
+              defaultValue={query}
+              onChangeText={text => this.setState({ query: text })}
+              placeholder="Enter tag"
+              renderItem={({ subject }) => (
+                <TouchableOpacity onPress={() => this.setState({ query: subject })}>
+                  <Text>
+                    {subject}
+                  </Text>
+                </TouchableOpacity>
+              )}
+              />
+*/
 
 const Styles = StyleSheet.create({
   container: {
@@ -140,8 +251,10 @@ const Styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   modalButton: {
-    borderRadius: 5,
+    borderRadius: 2,
     padding: 8,
+    paddingRight: 15,
+    paddingLeft: 15,
     textAlign: 'center',
     margin: 10,
     borderWidth: 1,
